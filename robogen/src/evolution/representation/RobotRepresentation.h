86ff2efc944dd5636f323018709bdf746c9cb745
Brooke <brooke44@live.co.za>
2020-09-02 13:58:41 +0200

    culmulativeWeightMap start

--- robogen/src/evolution/representation/RobotRepresentation.h
+++ robogen/src/evolution/representation/RobotRepresentation.h
@@ -252,6 +252,9 @@ public:
 	* BK: Genome for HyperNEAT-light attempt
 	*/
 	NEAT::Genome neatGenome;
+	
+	//BK culmulativeWeightMap for neural complexity 
+	 std::map<std::pair<std::string,std::string>, double> culmulativeWeightMap;
 
 private:
 	/**
