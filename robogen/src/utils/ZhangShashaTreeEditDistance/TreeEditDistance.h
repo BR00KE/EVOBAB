0d35409ffe829fb7c22e804b6932365cc94b93f0
Brooke <brooke44@live.co.za>
2020-09-06 18:46:31 +0200

    put all of Zhang-shasha classes in TreeEditDistance.h  because I was getting strange include path errors

--- robogen/src/utils/ZhangShashaTreeEditDistance/TreeEditDistance.h
+++ robogen/src/utils/ZhangShashaTreeEditDistance/TreeEditDistance.h
@@ -1,11 +1,37 @@
 #ifndef TREE_EDIT_DISTANCE
 #define TREE_EDIT_DISTANCE
 
-#include "utils/ZhangShashaTreeEditDistance/Tree.h"
-
+#include <vector>
+#include <string>
+#include <boost>
 namespace zhang_shasha {
 
+    //Node Class
+    class Node;
+
+    class Node{
+        public:
+            std::string label; //node label
+            int index; //preorder index
+            std::vector<zhang_shasha::Node> children;
+            zhang_shasha::Node leftmost; //used by recursive leftmost() function
+    };
    
+   //Tree Class
+    class Tree{
+        zhang_shasha::Node root;
+
+        std::vector<int> l;
+        //list of keyroots. i.e. nodes with a left child and the tree root
+        std::vector<int> keyroots;
+        std::vector<std::string> labels;
+
+        Tree(zhang_shasha::Node & root){ //construct the tree by passing a node for the root
+            this.root = root;
+        }
+
+        
+    };
 }
 
 #endif
